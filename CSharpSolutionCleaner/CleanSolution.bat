169a489839854ffb90f26e68bd58a606221f1b22
domasd <domas@dziaugys.com>
2017-01-04 12:00:32 +0200

    Clean solution

---
+++ CSharpSolutionCleaner/CleanSolution.bat
@@ -0,0 +1,6 @@
+rem deletes all bin and obj folders
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+rem deletes VS cache folder ".vs" 
+@RD /S /Q ".vs"
